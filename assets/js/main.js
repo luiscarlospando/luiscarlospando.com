7b3ade0f22ba69ebc8872b892ce162ba8a23929d
Luis Carlos Pando <hey@luiscarlospando.com>
2020-08-26 18:23:15 -0600

    Agregar timestamp del episodio más nuevo del Mode 7 Podcast a la página de Mode 7

--- assets/js/main.js
+++ assets/js/main.js
@@ -111,7 +111,7 @@
         // Retrieve latest post via API and fetch link
         $.get('https://blog.luiscarlospando.com/wp-json/wp/v2/posts?per_page=1', function (data) {
             // console.log(data);
-            $('#blog').append('<a href="' + data[0].link + '" data-toggle="tooltip" data-placement="top" title="' + data[0].title.rendered + '">' + data[0].title.rendered + '</a>');
+            $('#blog').append('<a href="' + data[0].link + '" data-toggle="tooltip" data-placement="top" title="' + data[0].title.rendered + '"><i class="fas fa-play"></i> ' + data[0].title.rendered + '</a>');
         });
 
         // Retrieve latest post from tag 'Mode 7 Podcast' via API and fetch link

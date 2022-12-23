c9500a359564dbf71913bfc83b6124453bc9df40
Luis Carlos Pando <hey@luiscarlospando.com>
2022-12-23 13:08:00 -0600

    Agregar plugin para fetchear RSS externos

--- assets/src/app.js
+++ assets/src/app.js
@@ -9,6 +9,7 @@ import 'bootstrap';
 import './jquery.mmenu.min.all';
 import './twitter-fetcher';
 import './lastfm.jquery';
+import './jquery.rss.min';
 import '../components/navigation';
 import '../components/lastTweet';
 import '../components/currentlyListening';

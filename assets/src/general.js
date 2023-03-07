8a0a92de6123c534c01acee290154f95199f9896
Luis Carlos Pando <hey@luiscarlospando.com>
2023-03-07 09:48:04 -0600

    Revertir cambios

--- assets/src/general.js
+++ assets/src/general.js
@@ -172,6 +172,33 @@
             $('#contador-posts').append(postCount);
         });
 
+        // Retrieve system status via Instatus API
+        $.get('https://luiscarlospando.instatus.com/summary.json', function (data) {
+            let btnSiteVersion = document.getElementById("site-version");
+            let status = data.page.status;
+            let systemStatus = "";
+
+            switch(status) {
+                case "UP":
+                    btnSiteVersion.classList.add("badge-success");
+                    systemStatus = '<i class="fa-solid fa-circle-check"></i> En funcionamiento';
+                    break;
+                case "HASISSUES":
+                    btnSiteVersion.classList.add("badge-danger");
+                    systemStatus = '<i class="fa-solid fa-circle-exclamation"></i> Hay problemas';
+                    break;
+                case "UNDERMAINTENANCE":
+                    btnSiteVersion.classList.add("badge-warning");
+                    systemStatus = '<i class="fa-solid fa-wrench"></i> En mantenimiento';
+                    break;
+                default:
+                    btnSiteVersion.classList.add("badge-info");
+                    systemStatus = '<i class="fa-solid fa-circle-minus"></i> Sin información';
+            }
+
+            $('#system-status').append(systemStatus);
+        });
+
         // Enabling Font Awesome Pseudo Elements
         window.FontAwesomeConfig = {
             searchPseudoElements: true

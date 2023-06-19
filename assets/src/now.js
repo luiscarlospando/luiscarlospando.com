faf1cbe73f412e8ce3b4eb3760f0388ad9cd4340
Luis Carlos Pando <hey@luiscarlospando.com>
2023-06-19 17:54:30 -0600

    Implementar página /now desde omg.lol

--- assets/src/now.js
+++ assets/src/now.js
@@ -1,6 +1,6 @@
 // Retrieve /now page via omg.lol
 $.get('https://api.omg.lol/address/mijo/now', function (data) {
-    let content = now.content;
+    let content = data.response.now.content;
         contentDom = document.getElementById("now-content");
 
         console.log(content);

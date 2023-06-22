2279596852a0aa2e4b61b4a43d59c240f83cc2fe
Luis Carlos Pando <hey@luiscarlospando.com>
2023-06-22 00:20:50 -0600

    Corregir bug cuando elemento last-updated-at no existe

--- assets/src/lastUpdatedAt.js
+++ assets/src/lastUpdatedAt.js
@@ -8,8 +8,13 @@ const relativeTime = require('dayjs/plugin/relativeTime');
 dayjs.extend(relativeTime);
 
 // Retrieve last updated date from posts that has it
-const lastUpdatedJekyll = document.getElementById("last-updated-at").innerHTML;
-let lastUpdatedRelative = dayjs().to(lastUpdatedJekyll);
+const lastUpdatedJekyll = document.getElementById("last-updated-at");
 
-// Update date with relative format
-document.getElementById("last-updated-at").innerHTML = lastUpdatedRelative;
+// Check if last updated at element exists, if is not null, then update date format
+if (lastUpdatedJekyll !== null) {
+    const lastUpdatedInner = document.getElementById("last-updated-at").innerHTML;
+    let lastUpdatedRelative = dayjs().to(lastUpdatedInner);
+
+    // Update date with relative format
+    document.getElementById("last-updated-at").innerHTML = lastUpdatedRelative;
+}

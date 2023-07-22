015643551b8cee081abff35a84db60644fc59c04
Luis Carlos Pando <hey@luiscarlospando.com>
2023-07-21 21:08:36 -0600

    Agregar badges de Twitch

--- assets/src/main.js
+++ assets/src/main.js
@@ -194,15 +194,21 @@
         const luisCarlosPandoChat = document.getElementById("luiscarlospando-chat");
         const btnLuisCarlosPando = document.getElementById("btn-luiscarlospando");
 
+        // Twitch badges
+        const badgeTwitchMijo = document.getElementById("badge-twitch-mijo");
+        const badgeTwitchSkynet = document.getElementById("badge-twitch-skynet");
+
         // Display livestream and livestream alert only on Thursday nights
         if (dayOfTheWeek == 4 && (time >= 21 && time < 23)) {
             if (m7gpLivestream && btnM7GP && luisCarlosPandoLivestream && luisCarlosPandoChat && btnLuisCarlosPando) {
                 m7gpLivestream.style.display = "block";
                 btnM7GP.style.display = "inline-block";
+                badgeTwitchSkynet.style.display = "inline-block";
 
                 luisCarlosPandoLivestream.style.display = "none";
                 luisCarlosPandoChat.style.display = "none";
                 btnLuisCarlosPando.style.display = "none";
+                badgeTwitchMijo.style.display = "none";
             }
 
             m7gpLivestreamAlert.style.display = "block";
@@ -210,6 +216,7 @@
             if (m7gpLivestream && btnM7GP) {
                 m7gpLivestream.style.display = "none";
                 btnM7GP.style.display = "none";
+                badgeTwitchSkynet.style.display = "none";
             }
 
             m7gpLivestreamAlert.style.display = "none";

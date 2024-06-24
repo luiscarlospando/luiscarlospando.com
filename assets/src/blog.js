229444138ce19be23aaf3fc03793b390849e4bdd
Luis Carlos Pando <hey@luiscarlospando.com>
2024-06-24 10:22:47 -0600

    Actualizar versión del sitio en el comment del header del blog

--- assets/src/blog.js
+++ assets/src/blog.js
@@ -1,6 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
   const btnVersionBlog = document.getElementById("btn-version-blog");
 
+  // Function to update the HTML comment
+  function updateComment(version) {
+    const comments = Array.from(
+      document.createTreeWalker(document, NodeFilter.SHOW_COMMENT),
+    );
+    comments.forEach((comment) => {
+      if (comment.nodeValue.includes("Site version: vPLACEHOLDER")) {
+        comment.nodeValue = `Site version: v${version}`;
+      }
+    });
+  }
+
   // Only proceed if the btn-version-blog element is found
   if (btnVersionBlog) {
     fetch("https://luiscarlospando.com/config.json")
@@ -21,6 +33,25 @@ document.addEventListener("DOMContentLoaded", function () {
         // Update innerHTML of the <code> tag
         const codeTag = btnVersionBlog.querySelector("code");
         codeTag.innerHTML = `v${data.site_version}`;
+
+        // Check for the presence of the PLACEHOLDER and update the HTML comment
+        const placeholderPresent = document.createTreeWalker(
+          document,
+          NodeFilter.SHOW_COMMENT,
+        );
+        let found = false;
+        while (placeholderPresent.nextNode()) {
+          if (
+            placeholderPresent.currentNode.nodeValue.includes("vPLACEHOLDER")
+          ) {
+            found = true;
+            break;
+          }
+        }
+
+        if (found) {
+          updateComment(data.site_version);
+        }
       })
       .catch((error) => {
         console.error("Error fetching config.json:", error);

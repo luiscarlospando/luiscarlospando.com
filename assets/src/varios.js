f3ded0207539c916dc9e3beeb1741837837a6efa
Luis Carlos Pando <hey@luiscarlospando.com>
2024-12-22 16:35:56 -0600

    Actualizar comentarios

--- assets/src/varios.js
+++ assets/src/varios.js
@@ -123,8 +123,8 @@
         // This code:
         // 1. Links containing target="_blank"
         // 2. It doesn't select links inside the header and footer
-        // 3. It doesn't select links containing the class .btn
-        // 4. It doesn't select links inside elements with class .mastodon
+        // 3. It doesn't select links with the class of .btn
+        // 4. It doesn't select links inside elements with the class of .mastodon
         const links = document.querySelectorAll('a[target="_blank"]');
 
         links.forEach((link, index) => {

0fdb8c0e6e55025c4aa8d14d73f98fa6719026b4
Luis Carlos Pando <hey@luiscarlospando.com>
2025-01-07 21:30:19 -0600

    Actualizar ícono del blog en el menú de navegación

--- assets/src/components/navigation.js
+++ assets/src/components/navigation.js
@@ -13,7 +13,7 @@ function Navigation() {
         {
             id: "2",
             url: "https://blog.luiscarlospando.com/",
-            icon: "fa-solid fa-comment",
+            icon: "fa-solid fa-rss",
             text: "Blog",
             subButtons: [
                 {

ef9978604cf6da4798d38cb21641b7717204fdbf
Luis Carlos Pando <hey@luiscarlospando.com>
2025-02-13 20:43:14 -0600

    Usar variable de entorno para API key de Last.fm

--- assets/src/lastfm-fetch.js
+++ assets/src/lastfm-fetch.js
@@ -1,41 +1,42 @@
-// Top 10 artists from Last.fmin the past month
+// Top 10 artists from Last.fm in the past month
 
 // Require dayjs from 'dayjs'
-const locale_es_mx = require('dayjs/locale/es-mx');
-const dayjs = require('dayjs');
-dayjs.locale('es-mx');
-const relativeTime = require('dayjs/plugin/relativeTime');
+const locale_es_mx = require("dayjs/locale/es-mx");
+const dayjs = require("dayjs");
+dayjs.locale("es-mx");
+const relativeTime = require("dayjs/plugin/relativeTime");
 dayjs.extend(relativeTime);
 
-// API URL (customized to get top 10 artists of the past month, 📃 docs: https://www.last.fm/api/show/user.getTopArtists)
-const lastfm_api = "https://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=luiscarlospando&api_key=28dd68a56fe0bebb7db5a287d6fdb4bc&format=json&period=1month&limit=10";
-
-// Using Promise syntax
+// Function to display Last.fm top artists
 function displayLastFmTopArtists() {
-  fetch(lastfm_api, {
-      method: "GET",
-      headers: {"Content-type": "application/json;charset=UTF-8"}})
-    .then(response => response.json())
-    .then(data => {
-      // Retrieve last 10 posts (specified on API URL), fetch links and display them on the /now page
-      let ready = (callback) => {
-        if (document.readyState != "loading") callback();
-        else document.addEventListener("DOMContentLoaded", callback);
-      }
-
-      ready(() => { 
-        /* Do things after DOM has fully loaded */
-        if (document.getElementById("lastfm-top-artists") !== null) {
-          console.log("✅ #lastfm-top-artists si existe en el DOM");
-          for (let i = 0; i < data.topartists.artist.length; i++) {
-              document.getElementById("lastfm-top-artists").innerHTML += `<li><a href="${data.topartists.artist[i].url}" target="_blank">${data.topartists.artist[i].name}</a></li>`;
-          }
-        } else {
-            console.log("❌ #lastfm-top-artists no existe en el DOM");
-        }
-      });
+    fetch("https://luiscarlospando.com/api/lastfmTopArtists", {
+        method: "GET",
+        headers: { "Content-type": "application/json;charset=UTF-8" },
     })
-    .catch(error => console.error(error));
+        .then((response) => response.json())
+        .then((data) => {
+            // Retrieve last 10 posts, fetch links and display them on the /now page
+            let ready = (callback) => {
+                if (document.readyState != "loading") callback();
+                else document.addEventListener("DOMContentLoaded", callback);
+            };
+
+            ready(() => {
+                /* Do things after DOM has fully loaded */
+                if (document.getElementById("lastfm-top-artists") !== null) {
+                    console.log("✅ #lastfm-top-artists si existe en el DOM");
+                    for (let i = 0; i < data.topartists.artist.length; i++) {
+                        document.getElementById(
+                            "lastfm-top-artists"
+                        ).innerHTML +=
+                            `<li><a href="${data.topartists.artist[i].url}" target="_blank">${data.topartists.artist[i].name}</a></li>`;
+                    }
+                } else {
+                    console.log("❌ #lastfm-top-artists no existe en el DOM");
+                }
+            });
+        })
+        .catch((error) => console.error(error));
 }
 
 // Function calls

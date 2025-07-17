afe210a06d174127650379414d11cfdd4ad73272
Luis Carlos Pando <hey@luiscarlospando.com>
2025-07-17 13:30:32 -0600

    Solucionar problemas con el lazy-loading y masonry

--- assets/src/masonry.js
+++ assets/src/masonry.js
@@ -3,12 +3,18 @@ import imagesLoaded from "imagesloaded";
 
 document.addEventListener("DOMContentLoaded", function () {
   document.querySelectorAll(".masonry-grid").forEach(function (grid) {
-    imagesLoaded(grid, function () {
-      new Masonry(grid, {
+    // Wait for images to load before Masonry init
+    imagesLoaded(grid, { background: true }, function () {
+      const msnry = new Masonry(grid, {
         itemSelector: ".grid-item",
         columnWidth: ".grid-sizer",
         percentPosition: true,
       });
+
+      // Extra: Relayout after slight delay for better accuracy
+      setTimeout(() => {
+        msnry.layout();
+      }, 500);
     });
   });
 });

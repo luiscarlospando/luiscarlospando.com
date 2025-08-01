1e2970b8a4a6632607c8c3a3b2667c2e68abce46
Luis Carlos Pando <hey@luiscarlospando.com>
2025-08-01 08:47:06 -0600

    Implementar validación en caso de que alertBar no exista en el DOM

--- assets/src/dismissable-alert.js
+++ assets/src/dismissable-alert.js
@@ -2,16 +2,20 @@ document.addEventListener("DOMContentLoaded", function () {
   const alertBar = document.getElementById("dismissable-alert");
   const closeButton = document.getElementById("alert-close");
 
-  // Show with fade-in
-  if (!localStorage.getItem("alertClosed")) {
-    setTimeout(() => {
-      alertBar.classList.add("show");
-    }, 100); // a small delay to ensure transition
-  }
+  if (alertBar) {
+    // Show with fade-in
+    if (!localStorage.getItem("alertClosed")) {
+      setTimeout(() => {
+        alertBar.classList.add("show");
+      }, 100); // a small delay to ensure transition
+    }
 
-  // Close with fade-out and save in localStorage
-  closeButton.addEventListener("click", function () {
-    alertBar.classList.remove("show");
-    localStorage.setItem("alertClosed", "true");
-  });
+    // Close with fade-out and save in localStorage
+    if (closeButton) {
+      closeButton.addEventListener("click", function () {
+        alertBar.classList.remove("show");
+        localStorage.setItem("alertClosed", "true");
+      });
+    }
+  }
 });

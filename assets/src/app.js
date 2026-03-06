e9e00438cfdfdcc00072610b40b1be75aa088cf7
Luis Carlos Pando <hey@luiscarlospando.com>
2026-03-06 11:22:56 -0600

    Implementar página /status

--- assets/src/app.js
+++ assets/src/app.js
@@ -14,6 +14,7 @@ import "./jquery.rss.min";
 import "./prism";
 import "./jquery.mmenu.min.all";
 import "./statuslog";
+import "./statuslog-archive";
 import "./last-updated-at";
 import "./blog-posts";
 import "./links";
